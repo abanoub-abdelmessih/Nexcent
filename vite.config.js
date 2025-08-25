38b3e445856820290c885223b2ee35c6dd35c542
abanoub-abdelmessih <abanoubabdelmessih110@gmail.com>
2025-08-26 01:06:35 +0300

    Fix Build Path

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
   plugins: [tailwindcss()],
+  base: "./",
+  build: {
+    rollupOptions: {
+      input: {
+        main: "./index.html",
+        about: "./about.html",
+      },
+    },
+  },
 });
